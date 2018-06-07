b9a61cbbe4e3bb3784c715fb2cbbccfd936a6581
4c <4c.mura@gmail.com>
2018-06-07 16:47:31 +0900

    init

---
+++ Assets/CubeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    //キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    private AudioSource blockAudio;
+
+
+	// Use this for initialization
+	void Start () {
+        blockAudio = GameObject.Find("BlockAudio").GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        if (transform.position.x < this.deadLine) {
+            Destroy(gameObject);
+        }
+
+	}
+
+    void OnCollisionEnter2D(Collision2D other) {
+        //print(other.gameObject.name);
+        // 岩のぶつかりを確認
+        if (other.gameObject.name != "UnityChan2D") {
+            blockAudio.Play();
+        }
+    }
+}
